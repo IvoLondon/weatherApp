a89e5a17d2f19cbe37b6c93ec66a2472f39c077c
Ivo <ivelin.iliev@dusted.com>
2019-01-03 10:37:48 +0000

    Installing Axios and getting API from weather station

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London,us&APPID=9c3d96c3b408d9a49408b71e7c131587')
+      .then(response => {
+        console.log(response.data.city.name);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div className="App">
